0c7c1d865bf63f50bdf4a3783dcfeca8b5f1603c
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-07-05 10:30:38 +0500

    done

---
+++ index.js
@@ -0,0 +1,25 @@
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+let fromselek = document.querySelector(".fromselek");
+let toselek = document.querySelector(".toselek");
+let h2 = document.querySelector("h2");
+
+button.addEventListener("click",(event) =>{
+  event.preventDefault();
+
+  let amount = Number(input.value);
+  let fromCur = fromselek.value;
+  let toCur = toselek.value;
+
+  fetch(`https://currency-converter-pro1.p.rapidapi.com/latest-rates?base=${fromCur}&currencies=${toCur}`,{
+    headers: {
+      "x-rapidapi-key": "fb323d50aemsh021cce6e22c7eccp14cda2jsn5f18a9634617"
+  }
+  })
+  .then((res) => res.json())
+  .then((data) =>{
+
+    let result = data.result[toCur.toUpperCase()]*amount
+    h2.textContent = result + toCur.toUpperCase(); 
+  });
+});
